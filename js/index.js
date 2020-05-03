370811e5722c477bf2c4632d7c7600976b414361
Soloman Garcia <soloman.garcia@gmail.com>
2020-05-02 19:18:18 -0600

    add images/debug

--- js/index.js
+++ js/index.js
@@ -22,9 +22,10 @@ const buildPosts = (blogPosts) => {
 
   for (blogPost of blogPosts) {
     const postDate = new Date(parseInt(blogPost.added_date)).toDateString();
+    const postImage = `${API_BASE_URL}${blogPost.post_image}`;
     blogPostscontent += `
     <div class="post">
-        <div class="post-image"></div>
+        <div class="post-image" style="background-image: url(${postImage})"></div>
         <div class="post-content">
           <div class="post-date">${postDate}</div>
           <div class="post-title"><h4>${blogPost.title}</h4></div>
