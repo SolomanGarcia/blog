0808b702fc3dd1febbfb3681c7abf24d0e5038fe
Soloman Garcia <soloman.garcia@gmail.com>
2020-05-03 14:17:19 -0600

    debug

--- js/post.js
+++ js/post.js
@@ -26,7 +26,7 @@ const getPost = () => {
 
 const buildPost = (data) => {
   const postDate = new Date(parseInt(data.added_date)).toDateString();
-  const postImage = `${API_BASE_URL}${data.post_image}`;
+  const postImage = `${API_BASE_URL}${data.post_image}`;  
   document.querySelector('header').style.backgroundImage = `url(${postImage})`;
   document.getElementById('individual-post-title').innerText = data.title;
   document.getElementById('individual-post-date').innerText = `Published on ${postDate}`;
